1a4f83486eb328fae2c743aa00c5cb25a4aecc0e
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-06-29 16:41:14 +0800

    Allow btn clicks to num, sign, and point to display

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+[... document.querySelectorAll(".calc-controls input.num-btn")].forEach(numBtn =>{
+    numBtn.addEventListener("click", inputNumber)
+});
+document.querySelector(".calc-controls input.num-sign").addEventListener("click", toggleNumSign);
+document.querySelector(".calc-controls input.num-point").addEventListener("click", addPoint);
+
+function toggleNumSign(){
+    const calcDisplay = document.querySelector(".calc-display");
+    if(Number(calcDisplay.textContent) === 0) return;
+    else if(calcDisplay.textContent.includes("-")) calcDisplay.textContent = calcDisplay.textContent.slice(1);
+    else calcDisplay.textContent = "-" + calcDisplay.textContent;
+}
+
+function addPoint(){
+    const calcDisplay = document.querySelector(".calc-display");
+    if(calcDisplay.textContent.includes(".")) return;
+    else if(calcDisplay.textContent === "") calcDisplay.textContent = "0.";
+    else calcDisplay.textContent +=  ".";
+}
+
+function inputNumber(e){
+    document.querySelector(".calc-display").textContent += e.target.value;
+}
+
 function add(num1 , num2){
     return num1 + num2;
 }
