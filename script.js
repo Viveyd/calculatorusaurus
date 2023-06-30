8ecb9ed8794372b4fb4c78af7f501c4ad73320f7
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-06-30 20:12:38 +0800

    Rounded results of multiply and divide

--- script.js
+++ script.js
@@ -154,11 +154,11 @@ function subtract(num1 , num2){
 }
 
 function multiply(num1 , num2){
-    return num1 * num2;
+    return roundTo2DP(num1 * num2);
 }
 
 function divide(num1 , num2){
-    return num1 / num2;
+    return roundTo2DP(num1 / num2);
 }
 
 function toggleNumSign(){
