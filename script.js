1ce0a19d99e83a4adc8a89e55ba9327ce1372c6c
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-06-30 08:31:08 +0800

    Rewrite operator and number input func, delete equate()

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.querySelector(".calc-controls input.clear-all").addEventListener("mouse
 
 
 window.addEventListener("keydown", (e) => {
-    const ACCEPTED = ["Enter", "0","1","2","3","4","5","6","7","8","9","/","*","-", "+", "."];
+    const ACCEPTED = ["Enter","0","1","2","3","4","5","6","7","8","9","/","*","-", "+", "."];
     if(ACCEPTED.includes(e.key)){
         if(e.key === "Enter") {
             document.querySelector(`.calc-controls input.num-equal`).click();
@@ -29,48 +29,41 @@ const calcInputs = {
     operator: null,
     temp: null,
     justCalculated: false,
+    lastInput: null
 }
 
+let calcResult = null;
+
 function setOperator(e){
-    if(calcInputs.temp === null) return;
-    else if(calcInputs.operand1 === null || calcInputs.justCalculated === true){
-        calcInputs.operand1 = calcInputs.temp;
+    const {operand1, operand2, operator, temp} = calcInputs;
+    const calcDisplay =  document.querySelector(".calc-display");
+    if(operand1 === null && temp){
+        calcInputs.operand1 = temp;
         calcInputs.operator = e.target.value;
-        clearTemp();
-        if(calcInputs.justCalculated === true) calcInputs.justCalculated = false;
-    } else {
-        calcInputs.operand2 = calcInputs.temp;
-        calcInputs.temp = operate(e.target.value, calcInputs.operand1, calcInputs.operand2);
-        calcInputs.operand1 = calcInputs.temp;
-        calcInputs.justCalculated = true;
-        document.querySelector(".calc-display").textContent = calcInputs.temp;
+        calcInputs.temp = null;
+    } else if(operand1 && operator && temp === null){
+        calcInputs.operator = e.target.value;
     }
+    else if(operand1 && operator && operand2 === null && temp){ // Computes result and changes operator
+        calcInputs.operand2 = temp;
+        calcResult = operate(operator, operand1, calcInputs.temp);
+        calcInputs.operator = e.target.value;
+        calcInputs.operand1 = calcResult;
+        calcInputs.operand2 = null;
+        calcDisplay.textContent = calcResult;
+        clearTemp();
+    } 
 }
 
 function equate(){ // for equal operator
-    if(calcInputs.temp === null) return;
-    else if(calcInputs.operand1 && calcInputs.operator){
-        calcInputs.operand2 = calcInputs.temp;
-        calcInputs.temp = operate(calcInputs.operator, calcInputs.operand1, calcInputs.operand2);
-        calcInputs.operand1 = null;
-        calcInputs.justCalculated = true;
-        document.querySelector(".calc-display").textContent = calcInputs.temp;
 
-    } else if(calcInputs.justCalculated){
-        calcInputs.temp = operate(calcInputs.operator, calcInputs.temp, calcInputs.operand2);
-        document.querySelector(".calc-display").textContent = calcInputs.temp;
-    }
 }
 
 function inputNumber(e){
-    if(calcInputs.temp === null || calcInputs.justCalculated) {
-        if(calcInputs.justCalculated === true){
-            calcInputs.justCalculated = false;
-        } 
-        calcInputs.temp = e.target.value;
-    }
+    if(calcInputs.temp === null) calcInputs.temp = e.target.value;
     else calcInputs.temp += e.target.value;
-    document.querySelector(".calc-display").textContent = calcInputs.temp;
+    const calcDisplay =  document.querySelector(".calc-display");
+    calcDisplay.textContent = calcInputs.temp;
 }
 
 function operate(operator, num1, num2){ // operator is one of the func above
@@ -140,3 +133,6 @@ function clearTemp(){
     calcInputs.temp = null;
 }
 
+/* 
+if click
+*/
